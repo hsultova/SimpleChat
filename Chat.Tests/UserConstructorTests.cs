fabbec834c237f972a36c98cfd60ea5db4d2d7cf
hsultova <hsultova@users.noreply.github.com>
2020-11-30 11:56:44 +0200

    Add User model and tests

---
+++ Chat.Tests/UserConstructorTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chat.Models;
+using NUnit.Framework;
+
+namespace Chat.Tests
+{
+    [TestFixture]
+    public class UserConstructorTests
+    {
+
+        [Test]
+        public void ConstructorTest()
+        {
+            //Arrange
+            string name = "TestName";
+            int id = 0;
+
+            //Act
+            var user = new User(id, name);
+
+            //Assert
+            Assert.That(user.Id.Equals(id));
+            Assert.That(user.Name.Equals(name));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        public void NoNameConstructorTest(string name)
+        {
+            //Arrange
+            int id = 0;
+
+            //Act
+            var user = new User(0, name);
+
+            //Assert
+            Assert.That(user.Id.Equals(id));
+            Assert.That(user.Name.Equals("Anonymous"));
+        }
+    }
+}
---
+++ Chat/Models/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Models
+{
+	public class User
+	{
+		public User(int id, string name)
+		{
+			Id = id;
+			Name = name;
+		}
+
+		public int Id { get; private set; }
+
+		private const string AnonymousName = "Anonymous";
+
+		private string _name;
+		public string Name
+		{
+			get
+			{
+				return _name;
+			}
+			private set
+			{
+				_name = value;
+				if (string.IsNullOrEmpty(_name))
+				{
+					_name = AnonymousName;
+				}
+			}
+		}
+
+	}
+}
