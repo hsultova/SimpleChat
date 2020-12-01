769bc3cf0624f9a7bf67f3eda20c6170dbc9b729
hsultova <hsultova@users.noreply.github.com>
2020-12-01 13:53:38 +0200

    Add MainWindowViewModel

---
+++ Chat/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Chat.Helpers;
+
+namespace Chat.ViewModels
+{
+	class MainWindowViewModel : INotifyPropertyChanged
+	{
+		public MainWindowViewModel()
+		{
+		}
+
+		public ObservableCollection<string> Messages { get; set; } = new ObservableCollection<string>();
+
+		public string MessageText { get; set; }
+
+		public string UserName { get; set; }
+
+		#region Commands
+
+		public ICommand Connect { get => new RelayCommand(OnConnect); }
+		public ICommand Send { get => new RelayCommand(OnSend); }
+
+		#endregion
+
+		private void OnConnect()
+		{
+		}
+
+		private void OnSend()
+		{
+		}
+
+		#region INotifyPropertyChanged
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void OnPropertyChanged([CallerMemberName] string propertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+		#endregion
+	}
+}
