b61a1a14ce1405a705cd4bd7b5be2d82c58d1b11
hsultova <hsultova@users.noreply.github.com>
2020-12-02 12:46:21 +0200

    Cleaning, view changes

--- Chat/ViewModels/MainWindowViewModel.cs
+++ Chat/ViewModels/MainWindowViewModel.cs
@@ -12,18 +12,57 @@ namespace Chat.ViewModels
 		{
 		}
 
+		#region INotifyPropertyChanged
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void OnPropertyChanged([CallerMemberName] string propertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+		#endregion
+
 		public ObservableCollection<string> Messages { get; set; } = new ObservableCollection<string>();
 
-		public string MessageText { get; set; }
+		private string _messageText;
+		public string MessageText
+		{
+			get
+			{
+				return _messageText;
+			}
+			set
+			{
+				_messageText = value;
+				OnPropertyChanged(nameof(MessageText));
+			}
+		}
 
-		public string UserName { get; set; }
+
+		private string _userName;
+		public string UserName
+		{
+			get
+			{
+				return _userName;
+			}
+			set
+			{
+				_userName = value;
+				OnPropertyChanged(nameof(UserName));
+			}
+		}
 
 		#region Commands
+
+		public ICommand EnterName { get => new RelayCommand(OnEnterName); }
 
 		public ICommand Connect { get => new RelayCommand(OnConnect); }
 		public ICommand Send { get => new RelayCommand(OnSend); }
 
 		#endregion
+
+
+		private void OnEnterName()
+		{
+		}
 
 		private void OnConnect()
 		{
@@ -32,12 +71,5 @@ namespace Chat.ViewModels
 		private void OnSend()
 		{
 		}
-
-		#region INotifyPropertyChanged
-
-		public event PropertyChangedEventHandler PropertyChanged;
-		private void OnPropertyChanged([CallerMemberName] string propertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
-		#endregion
 	}
 }
