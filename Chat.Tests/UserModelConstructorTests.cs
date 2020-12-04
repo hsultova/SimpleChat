2c6dc33f22b1beaa9f2887ffda10d00c316a6a68
hsultova <hsultova@users.noreply.github.com>
2020-12-04 14:08:28 +0200

    Cleaning
    Remove no needed usings

--- Chat.Tests/UserModelConstructorTests.cs
+++ Chat.Tests/UserModelConstructorTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Chat.Models;
+﻿using Chat.Models;
 using NUnit.Framework;
 
 namespace Chat.Tests
