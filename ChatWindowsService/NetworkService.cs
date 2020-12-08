5bb99a9f0d61c055aea6791c42ba974ee5d6a63b
hsultova <hsultova@users.noreply.github.com>
2020-12-08 19:37:19 +0200

    Add windows service to host the ChatService
    -Use tcp binding
    -Use mexTcpBinding for client/proxies generation
    -Add command-line arguments -install, -uninstall for easier installation/uninstallation

---
+++ ChatWindowsService/NetworkService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatWindowsService
+{
+	public partial class NetworkService : ServiceBase
+	{
+		public NetworkService()
+		{
+			InitializeComponent();
+		}
+
+		internal static ServiceHost host = null;
+
+		protected override void OnStart(string[] args)
+		{
+			if (host != null)
+			{
+				host.Close();
+			}
+			host = new ServiceHost(typeof(ChatService.ChatService));
+			host.Open();
+		}
+
+		protected override void OnStop()
+		{
+			if (host != null)
+			{
+				host.Close();
+				host = null;
+			}
+		}
+	}
+}
---
+++ ChatWindowsService/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration.Install;
+using System.Linq;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatWindowsService
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main(string[] args)
+		{
+			if (System.Environment.UserInteractive)
+			{
+				if (args.Count() == 1)
+				{
+					if (args[0] == "-install")
+						ManagedInstallerClass.InstallHelper(new string[] { Assembly.GetExecutingAssembly().Location });
+
+					if (args[0] == "-uninstall")
+						ManagedInstallerClass.InstallHelper(new string[] { "/u", Assembly.GetExecutingAssembly().Location });
+				}
+				return;
+			}
+
+			ServiceBase[] ServicesToRun;
+			ServicesToRun = new ServiceBase[]
+			{
+				new NetworkService()
+			};
+			ServiceBase.Run(ServicesToRun);
+		}
+	}
+}
