9b39369da0c7a4b87793797ea00f2fc90296d852
hsultova <hsultova@users.noreply.github.com>
2020-12-08 19:43:08 +0200

    Add client and service callbacks

---
+++ Chat/Generator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Helpers
+{
+	public static class Generator
+	{
+		public static int GenerateId()
+		{
+			return 0;
+		}
+	}
+}
--- Chat/ViewModels/MainWindowViewModel.cs
+++ Chat/ViewModels/MainWindowViewModel.cs
@@ -1,17 +1,32 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ServiceModel;
+using System.Windows;
 using System.Windows.Input;
 using Chat.Helpers;
+using Chat.Models;
 using Chat.ViewModels.Base;
 
 namespace Chat.ViewModels
 {
-	class MainWindowViewModel : BaseViewModel
+	[CallbackBehavior(UseSynchronizationContext = false, ConcurrencyMode = ConcurrencyMode.Multiple)]
+	class MainWindowViewModel : BaseViewModel, IDisposable, TcpServiceReference.IChatCallback
 	{
 		public MainWindowViewModel()
 		{
+			InstanceContext instanceContext = new InstanceContext(this);
+			_client = new TcpServiceReference.ChatClient(instanceContext);
 		}
 
+		public void Dispose()
+		{
+		}
+
+		private TcpServiceReference.ChatClient _client;
+
 		public ObservableCollection<string> Messages { get; set; } = new ObservableCollection<string>();
+
+		public UserModel CurrentUser { get; set; }
 
 		private string _messageText;
 		public string MessageText
@@ -38,6 +53,7 @@ namespace Chat.ViewModels
 			set
 			{
 				_userName = value;
+				CurrentUser = new UserModel(Generator.GenerateId(), UserName);
 				OnPropertyChanged(nameof(UserName));
 			}
 		}
@@ -58,10 +74,35 @@ namespace Chat.ViewModels
 
 		private void OnConnect()
 		{
+			var userModel = new UserModel(Generator.GenerateId(), UserName);
+			TcpServiceReference.User user = new TcpServiceReference.User { Id = userModel.Id, Name = userModel.Name };
+			_client.Connect(user);
 		}
 
 		private void OnSend()
 		{
+			TcpServiceReference.Message message = new TcpServiceReference.Message { Content = MessageText, DateTime = DateTime.Now, UserName = UserName };
+			_client.Send(message);
+		}
+
+		public void RefreshUsers(TcpServiceReference.User[] users)
+		{
+
+		}
+
+		public void Receive(TcpServiceReference.Message msg)
+		{
+			Application.Current.Dispatcher.Invoke(() => { Messages.Add(msg.Content); });
+		}
+
+		public void UserConnect(TcpServiceReference.User user)
+		{
+
+		}
+
+		public void UserDisconnect(TcpServiceReference.User user)
+		{
+
 		}
 	}
 }
