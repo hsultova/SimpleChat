6b699e97f0d172137f2d94caaf1b4a625303430f
hsultova <hsultova@users.noreply.github.com>
2020-12-08 19:12:26 +0200

    Add Messege model and basic tests for it

---
+++ Chat.Tests/MessageModelConstructorTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Chat.Models;
+using NUnit.Framework;
+
+namespace Chat.Tests
+{
+	[TestFixture]
+	public class MessageModelConstructorTests
+	{
+        [Test]
+        public void ConstructorTest()
+        {
+            //Arrange
+            string content = "Content";
+            DateTime dateTime = DateTime.Now;
+            string name = "TestName";
+
+            //Act
+            var message = new MessageModel(content, dateTime, name);
+
+            //Assert
+            Assert.That(message.Content.Equals(content));
+            Assert.That(message.DateTime.Equals(dateTime));
+            Assert.That(message.UserName.Equals(name));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        public void NoContentConstructorTest(string content)
+        {
+            //Arrange
+            DateTime dateTime = DateTime.Now;
+            string name = "TestName";
+
+            //Act
+            var message = new MessageModel(content, dateTime, name);
+
+            //Assert
+            Assert.That(message.Content.Equals(string.Empty));
+            Assert.That(message.DateTime.Equals(dateTime));
+            Assert.That(message.UserName.Equals(name));
+        }
+    }
+}
---
+++ Chat/Models/MessageModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Chat.Models
+{
+	public class MessageModel
+	{
+		public MessageModel(string content, DateTime dateTime, string userName)
+		{
+			Content = content ?? string.Empty;
+			DateTime = dateTime;
+			UserName = userName;
+		}
+
+		public string Content { get; set; }
+
+		public DateTime DateTime { get; set; }
+
+		public string UserName { get; set; }
+	}
+}
